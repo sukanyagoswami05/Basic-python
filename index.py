03ff9a17e8e396c341b430531762cedc911c9ef1
sukanyagoswami05 <sukanyagoswami889@gmail.com>
2025-05-15 20:10:23 +0530

    update

--- index.py
+++ index.py
@@ -3,3 +3,18 @@
 interpreter recognizes during the process of  lexical analysis(Breaking code into small parts (tokens) like words,
 numbers, symbols — so the computer can understand and execute it.)'''
 
+'''Tokens are
+1 Keyword 
+2 identifier
+3 literals
+4 Punctuation
+5 literals'''
+
+# KEYWORDS = these are the predefined words which means These are reserved words that have special meaning in Python.
+# You cannot use them as variable names. 
+
+import keyword
+all_keywords=keyword.kwlist
+print(all_keywords)
+print(len(all_keywords)) #there are total 35 predefined keywords 
+print(all_keywords)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+all_keywords=keyword.kwlist
