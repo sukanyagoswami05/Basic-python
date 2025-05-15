775b945ff2d31ad69f93c60358ea27ab1b71c519
sukanyagoswami05 <sukanyagoswami889@gmail.com>
2025-05-15 16:46:53 +0530

    update

--- index.py
+++ index.py
@@ -1,1 +1,5 @@
-print("hello world")
+# Tokens in python 
+'''In Python, a token is the smallest unit of the source code that the Python
+interpreter recognizes during the process of  lexical analysis(Breaking code into small parts (tokens) like words,
+numbers, symbols — so the computer can understand and execute it.)'''
+
