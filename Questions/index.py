842cf0b81fc1a2bd6aac2ef4b1005e035dd2ab36
sukanyagoswami05 <sukanyagoswami889@gmail.com>
2025-05-30 13:52:52 +0530

    update

--- Questions/index.py
+++ Questions/index.py
@@ -82,17 +82,15 @@ else :
  year = int(input("enter a year:"))
  #divided by 100 means century year (ending with 00)
  #century year divided by 400 is leap year 
- if (year % 400 == 0) and (year % 100 == 0):
-    print( "{0} is a leap year".format(year))
+
+      if (year % 400 == 0) and (year % 100 == 0):
+        print( "{0} is a leap year".format(year))
       # not divided by 100 means not a century year
       # year divided by 4 is a leap year
     elif: (year%4==0) and (year%100!==0):
     print("{} is a leap year")
         
 else: print("not a leap year")
-     
-     
-     
  year = int(input("Enter a year: "))
  # divided by 100 means century year (ending with 00)
  # century year divided by 400 is leap year
@@ -105,4 +103,5 @@ else: print("not a leap year")
  # if not divided by both 400 (century year) and 4 (not century year)
  # year is not leap year
  else:
- print("{0} is not a leap year".format(year))
+
+ 
