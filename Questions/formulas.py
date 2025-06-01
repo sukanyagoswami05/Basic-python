88ee09ccd0f4af7b4dc0452faaf46effc5fb1523
sukanyagoswami05 <sukanyagoswami889@gmail.com>
2025-06-01 23:28:57 +0530

    update

--- Questions/formulas.py
+++ Questions/formulas.py
@@ -1,3 +1,3 @@
-AREA OF TRIANGLE = 0.5 * base * height 
-Conversion factor: 1 kilometer = 0.621371 miles
-Conversion formula: Fahrenheit = (Celsius * 9/5) + 32
+# AREA OF TRIANGLE = 0.5 * base * height 
+# Conversion factor: 1 kilometer = 0.621371 miles
+# Conversion formula: Fahrenheit = (Celsius * 9/5) + 32
